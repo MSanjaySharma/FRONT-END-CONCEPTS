2a76d05cacf34da1878a3eeebf9ba0c52f6d5fb5
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-08-18 17:17:30 +0530

    Update JS Concepts

--- JAVASCRIPT/18.es6-features/test.js
+++
@@ -1,7 +0,0 @@
-//there is no easy way from earth to stars
-const arr = new Array();
-arr.push(2, 3);
-arr.push(4);
-console.log(arr);
-var prototype = Object.getPrototypeOf(Object);
-console.log(prototype);
--- JAVASCRIPT/18.es6-features/test2.js
+++
@@ -1,5 +0,0 @@
-let username = "BadUs3rnam3";
-let userCheck = /[a-z]+[0-9]+$/i;
-let result = userCheck.test(username);
-
-console.log(result);
